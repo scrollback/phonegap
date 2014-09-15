386d6925b87adf6c617ba8b600f11bc9db7685c0
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-09-15 17:38:12 +0530

    Fix files for phonegap app

--- www/index.js
+++ www/index.js
@@ -119,8 +119,7 @@ function onNotificationGCM(e) {
 			// Your GCM push server needs to know the regID before it can push to this device
 			// here is where you might want to send it the regID for later use.
 			alert("regID = " + e.regid);
-			alert("regID = " + e.regid);
-			document.write(e.regid);
+			
 		}
 	break;
 
