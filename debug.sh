af29ca4f1d6dffa58e9edc6cbb26cd512486cd6a
Amal Antony <amalantony06@gmail.com>
2014-12-09 16:24:57 +0530

    Fixed debug script.

--- debug.sh
+++ debug.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cordova build android
 unzip platforms/android/ant-build/Scrollback-debug.apk assets/www/* -d tmp/
-mv -v tmp/assets/www/cordova.js cordova-plugins/defaults/
-mv -v tmp/assets/www/cordova_plugins.js cordova-plugins/defaults/
-mv -f -v tmp/assets/www/plugins/ cordova-plugins/defaults/
+mv -v tmp/assets/www/cordova.js cordova-plugins/defaults/cordova.js
+mv -v tmp/assets/www/cordova_plugins.js cordova-plugins/defaults/cordova_plugins.js
+mv -f -v tmp/assets/www/plugins cordova-plugins/defaults/
 rm -r -v tmp
-scp -C -r ./cordova-plugins scrollback@stage.scrollback.io:~/scrollback/public/phonegap/
+scp -C -r ./cordova-plugins/defaults scrollback@stage.scrollback.io:~/scrollback/public/phonegap/cordova-plugins/
 cordova run android
